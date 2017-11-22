4dfe4e88cb82e45ab3520f498d7925fe146dd973
tcbegley <tom@asidatascience.com>
2017-11-22 23:05:05 +0000

    Completed implementation of knights (needs some documentation).

--- Misc/knights.py
+++ Misc/knights.py
@@ -33,45 +33,89 @@ class Graph(object):
         """
         Conducts breadth first search for vertex target starting from vertex
         start. Returns length of shortest path, or -1 if target is unreachable
-        from start.
+        from start, and optimal route. This function is just a wrapper that
+        processes the returned dictionary.
         """
+        depth, path_dict = self._bfs(start, target)
+        best_path = []
+        if path_dict:
+            k = target
+            best_path.append(k)
+            while k != start:
+                k = path_dict[k]
+                best_path.append(k)
+        return depth, best_path[::-1]
+
+    def _bfs(self, start, target):
+        """Implements the actual breadth first search."""
         visited = defaultdict(lambda: False)
 
         queue = [start]
         visited[start] = True
 
         depth = {}
+        reached_from = {}
         depth[start] = 0
 
         while queue:
             s = queue.pop(0)
             d = depth[s]
             if s == target:
-                return d
+                return d, reached_from
             for i in self.graph[s]:
                 if not visited[i]:
                     visited[i] = True
                     queue.append(i)
                     depth[i] = d + 1
-        return -1
+                    reached_from[i] = s
+        return -1, None
 
 
 class Knight(object):
     def __init__(self):
         self.board, self.vertex_map = generate_board()
 
+    def shortest_path(self, start, stop):
+        start_i = self.vertex_map.index(start)
+        stop_i = self.vertex_map.index(stop)
+        _, best_path = self.board.bfs(start_i, stop_i)
+        return [self.vertex_map[i] for i in best_path]
+
 
 def generate_board():
     """Generate graph of possible knight moves and lookup dictionary"""
-    pass
+    vertex_map = [b+a for b in 'ABCDEFGH' for a in '12345678']
+    board = Graph()
+    for i in range(8):
+        for j in range(8):
+            loc = 8*i + j
+            if i >= 2 and j > 0:
+                # up 2, left 1
+                board.add_edge(loc, loc-17)
+            if i >= 2 and j < 7:
+                # up 2, right 1
+                board.add_edge(loc, loc-15)
+            if i >= 1 and j > 1:
+                # up 1, left 2
+                board.add_edge(loc, loc-10)
+            if i >= 1 and j < 6:
+                # up 1, right 2
+                board.add_edge(loc, loc-6)
+            if i < 7 and j > 1:
+                # down 1, left 2
+                board.add_edge(loc, loc+6)
+            if i < 7 and j < 6:
+                # down 1, right 2
+                board.add_edge(loc, loc+10)
+            if i < 6 and j > 0:
+                # down 2, left 1
+                board.add_edge(loc, loc+15)
+            if i < 6 and j < 7:
+                # down 2, right 1
+                board.add_edge(loc, loc+17)
+    return board, vertex_map
 
 
 if __name__ == "__main__":
-    g = Graph()
-    g.add_edge('a', 1)
-    g.add_edge('a', 2)
-    g.add_edge(1, 2)
-    g.add_edge(2, 'a')
-    g.add_edge(2, 3)
-    g.add_edge(3, 3)
-    print(g.bfs(1, 'a'))
+    k = Knight()
+    print(k.shortest_path('A1', 'D4'))
